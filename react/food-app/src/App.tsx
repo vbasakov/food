94cbd76551cf3cd2ec371ace57df9cfd971f1b88
vbasakov <vbasakov@gmail.com>
2023-12-22 00:50:27 +0300

    merge wip

--- react/food-app/src/App.tsx
+++ react/food-app/src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
 import $ from 'jquery';
-
-$("button").click(function(){
-    $.get("demo_test.asp", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
-});
+import {Food} from "./food";
 
 function App() {
-  return (
-    <div className="App">
-      <div id="div1">
-        <h2>Let jQuery AJAX Change This Text</h2>
-      </div>
-      <button>Get External Content</button>
-    </div>
+    return (
+        Food()
   );
 }
 
